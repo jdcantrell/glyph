aa9727dadcb4a549e288a9bc4c4e253973e71bbe
JD Cantrell <jd@goodrobot.net>
2016-01-18 22:40:34 -0800

    Add stupid and simple keyboard selection

--- glyph.js
+++ glyph.js
@@ -1,8 +1,8 @@
 var data = [
-  {type:"heading", content: 'Christmas and Cookies'},
+  {type: "heading", content: 'Christmas and Cookies'},
   {
     type: 'text',
-    content:'Here is some text about these set of photos. We had a good time and did many things together. I think we\'ll do it all over again someday.'
+    content: 'Here is some text about these set of photos. We had a good time and did many things together. I think we\'ll do it all over again someday.'
   },
   {type: 'img', src: 'images/P1000795.JPG', width: 689, height: 517},
   {
@@ -11,8 +11,8 @@ var data = [
       {type: 'scaled-img', src: 'images/P1000796.JPG', width: 689, height: 517},
       {type: 'scaled-img', src: 'images/P1000800.JPG', width: 689, height: 517},
       {type: 'scaled-img', src: 'images/P1000801.JPG', width: 689, height: 517},
-      {type: 'placeholder', src: 'images/P1000801.JPG', width: 689, height: 517},
-    ],
+      {type: 'scaled-img', src: 'images/P1000801.JPG', width: 689, height: 517, modifiers: ['highlight']}
+    ]
   },
   {
     type: 'photo-row',
@@ -20,22 +20,27 @@ var data = [
       {type: 'scaled-img', src: 'images/P1000802.JPG', width: 689, height: 517},
       {type: 'scaled-img', src: 'images/P1000803.JPG', width: 689, height: 517},
       {type: 'scaled-img', src: 'images/P1000804.JPG', width: 517, height: 689},
-      {type: 'scaled-img', src: 'images/P1000805.JPG', width: 689, height: 517},
-    ],
+      {type: 'scaled-img', src: 'images/P1000805.JPG', width: 689, height: 517}
+    ]
   },
   {
     type: 'photo-row',
     content: [
       {type: 'scaled-img', src: 'images/P1000806.JPG', width: 517, height: 689},
-      {type: 'scaled-img', src: 'images/P1000807.JPG', width: 517, height: 689},
-    ],
+      {type: 'scaled-img', src: 'images/P1000807.JPG', width: 517, height: 689}
+    ]
   },
   {type: 'text', content: 'We also did some other things that were neat. You can see more about them below'},
   {type: 'img', src: 'images/P1000808.JPG', width: 517, height: 689},
-  {type: 'img', src: 'images/P1000809.JPG', width: 517, height: 689},
+  {type: 'img', src: 'images/P1000809.JPG', width: 517, height: 689}
 ];
 
-var ph = data[3].content[3];
+var modifiers = {
+  'highlight': function (data, el) {
+    el.classList.add('highlight');
+    return el;
+  }
+};
 
 var renderers = {
   'heading': function (data) {
@@ -45,14 +50,14 @@ var renderers = {
   },
   'scaled-img': function (data) {
     var img = document.createElement('img');
-    img.setAttribute('src', data.src)
+    img.setAttribute('src', data.src);
     img.style.width = data.width + 'px';
     img.style.height = data.height + 'px';
     return img;
   },
   'img': function (data) {
     var img = document.createElement('img');
-    img.setAttribute('src', data.src)
+    img.setAttribute('src', data.src);
     return img;
   },
   'text': function (data) {
@@ -75,19 +80,19 @@ var renderers = {
 
     var full_width = 0;
     data.content.forEach(function (item) {
-      full_width += (max_y / item.height) * item.width
+      full_width += (max_y / item.height) * item.width;
     });
 
     var scaling = (800 / full_width);
 
     var total_width = 0;
-    var html = data.content.map(function (item, idx) {
+    var html = data.content.map(function (item) {
       item.parent = data;
-      y_scale = (max_y / item.height);
-      scale = scaling * y_scale;
+      var y_scale = (max_y / item.height);
+      var scale = scaling * y_scale;
 
       var width = Math.round(item.width * scale);
-      total_width += width
+      total_width += width;
 
       //make sure rounding keeps us in safe spot
       if (total_width > 800) {
@@ -99,41 +104,45 @@ var renderers = {
       return render_item(item);
     });
 
-    var div = document.createElement('div')
-    div.classList.add('photo-row')
+    var div = document.createElement('div');
+    div.classList.add('photo-row');
     html.forEach(function (el) { div.appendChild(el); });
 
-    return div
+    return div;
   }
 };
 
 
-render_map = {};
-key = 0;
+var render_map = {};
+var key = 0;
 
 function render_item(item) {
   if (!item.key) {
     item.key = key;
     render_map[key] = item;
 
-    key +=1;
+    key += 1;
   }
-
 
   var el = renderers[item.type](item);
   el.setAttribute('data-key', item.key);
+  if (item.modifiers) {
+    item.modifiers.forEach(function (modifier) {
+      modifiers[modifier](item, el);
+    });
+  }
   return el;
 }
 
 function render_content(data) {
   window.requestAnimationFrame(function () {
     var html = data.map(function (item) {
-      return render_item(item)
+      return render_item(item);
     });
 
-   var container = document.querySelector('.glyph-content')
-   container.innerHTML = '';
-   html.forEach(function (el) { container.appendChild(el); });
+    var container = document.querySelector('.glyph-content');
+    container.innerHTML = '';
+    html.forEach(function (el) { container.appendChild(el); });
   });
 }
 
@@ -150,31 +159,120 @@ function remove(item) {
   items = items.splice(idx, 1);
 }
 
-
-var last_key = null;
-function move_placeholder(event) {
-  var key = event.target.dataset.key;
-  if (key && last_key && key !== last_key) {
-    var item = render_map[key];
-    if (item !== ph) {
-      remove(ph);
-      move_after(ph, item);
-      render_content(data);
-    }
-    last_key = null;
-    return;
-  }
-  last_key = key;
-}
-
-function motion() {
- var container = document.querySelector('.glyph-content')
- container.addEventListener('mousemove', move_placeholder);
-
+function find(data, attrs) {
+  console.log('find', data, attrs);
+  return data.find(function (item) {
+    return Object.keys(attrs).every(function (key) {
+      if (item[key]) {
+        return (attrs[key] === item[key] || item[key].indexOf(attrs[key]) !== -1);
+      }
+      return false;
+    }) || (item.content && item.content.find? typeof find(item.content, attrs) !== 'undefined' : false);
+  });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
   render_content(data);
+});
 
-  motion();
+var state = {
+  mode: 'select',
+  selected: null,
+  get_selected: function () {
+    if (this.selected === null) {
+      this.selected = find(data, {modifiers: 'highlight'});
+    }
+    return this.selected;
+  }
+};
+
+var modes = {
+  select: {
+    up: function (data) {
+      var item = state.get_selected();
+      if (item.parent) {
+        item = item.parent;
+      }
+
+      //no nesting so everything is on the top level
+      var idx = data.indexOf(item);
+
+      if (idx > 1) {
+        var new_selection = data[idx - 1];
+        if (Array.isArray(new_selection.content)) {
+          new_selection = new_selection.content[0];
+        }
+        state.selected.modifiers = [];
+        new_selection.modifiers = ['highlight'];
+        state.selected = new_selection;
+        render_content(data);
+      }
+    },
+    down: function (data) {
+      var item = state.get_selected();
+      if (item.parent) {
+        item = item.parent;
+      }
+
+      //no nesting so everything is on the top level
+      var idx = data.indexOf(item);
+
+      if (idx < data.length - 1) {
+        var new_selection = data[idx + 1];
+        if (Array.isArray(new_selection.content)) {
+          new_selection = new_selection.content[0];
+        }
+
+        state.selected.modifiers = [];
+        new_selection.modifiers = ['highlight'];
+        state.selected = new_selection;
+        render_content(data);
+      }
+    },
+    right: function (data) {
+      var item = state.get_selected();
+      if (item.parent) {
+        var row_data = item.parent.content;
+        var idx = row_data.indexOf(item);
+        if (idx < row_data.length - 1) {
+          var new_selection = row_data[idx + 1];
+          state.selected.modifiers = [];
+          new_selection.modifiers = ['highlight'];
+          state.selected = new_selection;
+          render_content(data);
+        }
+      }
+    },
+    left: function (data) {
+      var item = state.get_selected();
+      if (item.parent) {
+        var row_data = item.parent.content;
+        var idx = row_data.indexOf(item);
+        if (idx > 1) {
+          var new_selection = row_data[idx - 1];
+          state.selected.modifiers = [];
+          new_selection.modifiers = ['highlight'];
+          state.selected = new_selection;
+          render_content(data);
+        }
+      }
+    }
+  }
+};
+
+document.addEventListener('keyup', function (event) {
+  switch (event.keyCode) {
+    case 38:
+      modes[state.mode].up(data);
+      break;
+    case 40:
+      modes[state.mode].down(data);
+      break;
+    case 37:
+      modes[state.mode].left(data);
+      break;
+    case 39:
+      modes[state.mode].right(data);
+      break;
+  }
 });
